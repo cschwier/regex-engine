d1e3f222e75efd3c1ff1536310b3e499e82f6a60
Schwier, Christian <christian.schwier@accenture.com>
2025-07-02 11:29:07 +0200

    test: add wildcard tests

---
+++ test/wildcard_test.py
@@ -0,0 +1,17 @@
+import unittest
+
+from src.regex import Parser
+
+
+class RegexWildcardTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.testee = Parser("a.c").as_predicate()
+
+    def test_literal_acceptance(self):
+        self.assertTrue(self.testee("abc"))
+
+    def test_dot_acceptance(self):
+        self.assertTrue(self.testee("a.c"))
+
+    def test_question_mark_acceptance(self):
+        self.assertTrue(self.testee("a?c"))
