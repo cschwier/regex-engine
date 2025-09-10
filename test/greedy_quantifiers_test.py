17078d5076e1e5f053ab91f7516e1904e1e072d4
Schwier, Christian <christian.schwier@accenture.com>
2025-09-10 12:22:45 +0200

    test: add new test stubs

---
+++ test/greedy_quantifiers_test.py
@@ -0,0 +1,28 @@
+import unittest
+
+from regex import Parser
+
+
+class StarTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.testee = ...
+
+class PlusTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.testee = ...
+
+class QuestionMarkTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.testee = ...
+
+class ExactlyEnTimesTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.testee = ...
+
+class EnOrMoreTimesTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.testee = ...
+
+class EnToEmTimesTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.testee = ...
