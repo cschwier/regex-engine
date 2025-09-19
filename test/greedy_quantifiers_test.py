67b79f4d00270a7f200cea55ecc2f99b9220d164
Schwier, Christian <christian.schwier@accenture.com>
2025-09-19 15:00:25 +0200

    test: {n, m} test case

--- test/greedy_quantifiers_test.py
+++ test/greedy_quantifiers_test.py
@@ -28,4 +28,17 @@ class EnOrMoreTimesTest(unittest.TestCase):
 
 class EnToEmTimesTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.testee = ...
+        self.testee = Parser("ab{2,3}b{2,3}").as_predicate()
+
+    def test_too_little(self):
+        assert not self.testee("abbb")
+
+    def test_min(self):
+        assert self.testee("abbbb")
+
+    def test_max(self):
+        assert self.testee("abbbbbb")
+
+    def test_too_many(self):
+        assert not  self.testee("abbbbbbb")
+
